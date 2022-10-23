c7c88fc7c971a1f28fa50b1bc49da62f184b1e09
Jason-Wang1245 <jasonwang1245@gmail.com>
2022-10-23 15:55:24 -0400

    updated comments

--- crawler.py
+++ crawler.py
@@ -9,31 +9,34 @@ def readPage(seed):
     webData = webdev.read_url(seed)
     folderPath = os.path.join("pageData", seed[7:].replace("/", "}"))
     os.makedirs(folderPath)
+    # local variables that stores data later to be written to files
     data = {}
     tf = {}
-    numWords = 0
     referenceLinks = {"referenceLinks": []}
+    numWords = 0
     i = 0
+    # iterates through html code of the given seed
     while i < len(webData):
         # gets the title of the page
         if webData[i:i+7] == "<title>":
             title = webData[i+7:webData.find("</title>")]
             i = webData.find("</title>") + 8
-        # finds every paragraph tag and checks the link that those link direct to
+        # finds every paragraph tag
         if webData[i:i+2] == "<p":
             words = webData[webData.find(">", i) + 1:webData.find("</p>", i)].strip().split()
-            # adds all words within the paragraph tag to fileContents
+            # add all words within the paragraph tag to data (counter system using dictionary)
             for word in words:
                 word = word.lower()
                 if word in data:
                     data[word] += 1
                 else:
                     data[word] = 1
-            # adds the total of words just added to fileContents
+            # adds the total number of words just found within the paragraph tag to numWords
             numWords += len(words)
-            # makes next iteration skips the found data
+            # makes next iteration skip the found data
             i = webData.find("</p>", i) + 4
             continue
+        # finds every anchor tag
         if webData[i:i+2] == "<a":
             href = webData[webData.find("href=\"", i) + 6:webData.find("\">", i)]
             # checks if the anchor tag is a relative link
@@ -41,26 +44,27 @@ def readPage(seed):
                 fullpath = seed[:seed.rfind("/") + 1] + href[2:]
             else:
                 fullpath = href
-            # adds outgoing links to outgoingLinks, which will be stored as a file later
+            # adds outgoing links to the according page in outgoingLinks, which will be stored as a file later
             if fullpath in outgoingLinks:
                 outgoingLinks[fullpath].append(seed)
             else:
                 outgoingLinks[fullpath] = [seed]
-            # add reference link to fileContents
+            # adds link to referenceLinks
             referenceLinks["referenceLinks"].append(fullpath)
             # ensures that recursion only visits each unique website once
             if not fullpath in checkedPages:
+                # index used to identify elements within adjacency matrix when calculating page rank
                 global index
                 index += 1
                 checkedPages[fullpath] = index
                 # makes next iteration skip the rest of the anchor tag that has already been found
                 i = webData.find("</a>", i) + 4
                 readPage(fullpath)
-
         i += 1
-    # adds tf values to the fileContents
+
+    # adds tf values to the variable tf
     for word in data:
-        # add the number of times each word appears in each webpage
+        # add the number of times each word appears in each webpage (later used to calculate idf)
         if word in wordCounter:
             wordCounter[word] += 1
         else:
@@ -68,15 +72,17 @@ def readPage(seed):
         # calculates tf score for each word within the current seed (webpage)
         tf[word] = data[word] / numWords
 
+    # seperates each calculated data seciton to their according file within the webpage folder
     fileWrite = open(os.path.join(folderPath, "tf.json"), "w")
     json.dump(tf, fileWrite)
     fileWrite = open(os.path.join(folderPath, "referenceLinks.json"), "w")
     json.dump({"referenceLinks": referenceLinks["referenceLinks"]}, fileWrite)
     fileWrite = open(os.path.join(folderPath, "title.txt"), "w")
     fileWrite.write(title)
+    fileWrite.close()
 
 
-# clears all local cache files and folders
+# clears all local cached files and folders
 def removeSavedData():
     if os.path.exists("pageData"):
         dataFolders = os.listdir("pageData")
@@ -93,7 +99,7 @@ def removeSavedData():
             os.remove(os.path.join("idf", file))
         os.rmdir("idf")
 
-# saves idf values within a json file
+# calculate and save idf values within a json file
 def saveIdf():
     for word in wordCounter:
         idf[word] = math.log(len(checkedPages) / (1 + wordCounter[word]), 2)
@@ -104,27 +110,28 @@ def saveIdf():
 # calculates the page rank of each page and saves it to global variable pageRank
 def calculatePageRank():
     pageFolders = os.listdir("pageData")
+    # creates the initial vector with sum of all elements adding up to 1 (later used to calculate euclidian distance for page rank score)
     vectorA = [[1/len(checkedPages)] * len(checkedPages)]
     euclideanDistance = 1
     # let N represent the number of webpages
     # creates a matrix that has N indices
     adjcencyMatrix = [0] * len(checkedPages)
-    # adds 1 to each index where a page references another
+    # adds 1 / N to each index where a page references another
     for folder in pageFolders:
         folderPath = os.path.join("pageData", folder)
         name = "http://" + folder.replace("}", "/")
         fileRead = open(os.path.join(folderPath, "referenceLinks.json"), "r")
         referenceLinks = json.load(fileRead)["referenceLinks"]
+        # creates the rows for the matrix with length of N
         row = [0] * len(checkedPages)
         for link in referenceLinks:
-            # adds 1 to each index that represents an outgoing reference
             row[checkedPages[link]] = 1 / len(referenceLinks)
         # adds the row into the corresponding index of the current page (name)
         adjcencyMatrix[checkedPages[name]] = row
 
     # multiples entire matrix by 1 - alpha(0.1)
     adjacencyMatrix = matmult.mult_scalar(adjcencyMatrix, 1 - 0.1)
-    # adds alpha(0.1)/N
+    # adds alpha(0.1)/N to entire matrix
     for i in range(len(checkedPages)):
         for j in range(len(checkedPages)):
             adjacencyMatrix[i][j] += 0.1 / len(checkedPages)
@@ -136,11 +143,11 @@ def calculatePageRank():
         vectorB = matmult.mult_matrix(vectorA, adjacencyMatrix)
         euclideanDistance = matmult.euclidean_dist(vectorA, vectorB)
     
-    # saves calculated page rank values to a pageRank
+    # saves calculated page rank values to pageRank
     for folder in pageFolders:
         pageRank[folder] = vectorB[0][checkedPages["http://" + folder.replace("}", "/").strip(".json")]]
 
-# writes all the additional values to each individual webpages json cache file
+# writes all the additional values to each accoding files within each webpage folder
 def writeAdditionalData():
     pageFolders = os.listdir("pageData")
     for folder in pageFolders:
@@ -148,19 +155,18 @@ def writeAdditionalData():
         fileRead = open(os.path.join(folderPath, "tf.json"), "r")
         tf = json.load(fileRead)
         fileRead.close()
-        # calculate tf-idf for current page and add it to the current page json data
+        # calculate tf-idf for current page and add it to the current page data folder
         tfIdf = {}
         for word in tf:
             tfIdf[word] = math.log(1 + tf[word], 2) * idf[word]
         fileWrite = open(os.path.join(folderPath, "tf-idf.json"), "w")
         json.dump(tfIdf, fileWrite)
-        # adds page rank and outgoing links to the current page json data
+        # adds page rank and outgoing links to the current page data folder
         fileWrite = open(os.path.join(folderPath, "pageRank.txt"), "w")
         fileWrite.write(str(pageRank[folder]))
         fileWrite = open(os.path.join(folderPath, "outgoingLinks.json"), "w")
         json.dump({"outgoingLinks": outgoingLinks["http://" + folder.replace("}", "/").strip(".json")]}, fileWrite)
         fileWrite.close()
-    
 
 # crawls all webpages within in the given seed page and store data in json files
 def crawl(seed):
@@ -190,10 +196,8 @@ def crawl(seed):
     os.makedirs("idf")
     # calls readPage to crawl webpages
     readPage(seed)
-    # saves additional values of data
+    # saves and calcualtes additional values
     saveIdf()
     calculatePageRank()
     writeAdditionalData()
     return(len(checkedPages))
-
-crawl("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html")
--- search.py
+++ search.py
@@ -2,7 +2,7 @@ import math
 import os
 import searchdata
         
-# altered selection sort to sort top 10 (or all if list is less than 10) consine similarity scores
+# altered selection sort to sort top 10 (or all of the list if length of list is less than 10) based on consine similarity scores
 def selectionSort(list):
     start = 0
     if len(list) < 10:
@@ -15,7 +15,6 @@ def selectionSort(list):
                 list[i], list[start] = list[start], list[i]
         start += 1
     return list[0:listMax]
-
 
 def search(phrase, boost):
     # final list to be returned
@@ -31,19 +30,20 @@ def search(phrase, boost):
     for word in phraseSplit:
         if not word in words:
             words[word] = phraseSplit.count(word)
-    # generates the order of the vector to be made with each page file, queryDenominator, and the queryVector to be used to calculate the numerator of the cosine similarity equation
+    # generates the order of the vector to be used when making each page vector, queryDenominator, and the queryVector to be used to calculate the numerator of the cosine similarity equation
     for word in words:
         idf = searchdata.get_idf(word)
+        # this skips words in the query that do not exist in crawled pages
         if idf != 0:
             queryTfidf = math.log(1 + (words[word] / len(phraseSplit)), 2) * idf
             queryVector.append(queryTfidf)
             vectorOrder.append(word)
             queryDenominator += queryTfidf ** 2
     queryDenominator = queryDenominator ** (1 / 2)
-    # gets every webpage data file in order to generate cosine similarities to the query vector
-    pageFiles = os.listdir("pageData")
-    for file in pageFiles:
-        url = "http://" + file.replace("}", "/").rstrip(".json")
+    # gets every webpage data folder in order to generate cosine similarities to the query vector
+    pageFolders = os.listdir("pageData")
+    for folder in pageFolders:
+        url = "http://" + folder.replace("}", "/").rstrip(".json")
         pageDenominator = 0
         numerator = 0
         # generates rest of the values needed to calculate cosine similarity
